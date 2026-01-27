2ff2b100bdbaa3613f4c3a290c6f1942a56d42f3
sajad <sajadath1@gmail.com>
2026-01-27 23:19:00 +0330

    change react query options

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -4,7 +4,23 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactNode, useState } from "react";
 
 export default function Providers({ children }: { children: ReactNode }) {
-  const [client] = useState(() => new QueryClient());
+  const [client] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            retry: 1,
+            staleTime: 1000 * 60 * 3,
+            gcTime: 1000 * 60 * 20,
+            refetchOnWindowFocus: false,
+            refetchOnMount: false,
+          },
+          mutations: {
+            retry: 0,
+          },
+        },
+      }),
+  );
 
   return <QueryClientProvider client={client}>{children}</QueryClientProvider>;
 }
